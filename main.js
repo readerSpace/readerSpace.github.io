e99a68fbb7ff9b37dda701b903540e5338c5b2b9
readerSpace <keks56496@mineo.jp>
2022-10-15 17:28:14 +0900

    make task list

--- main.js
+++ main.js
@@ -5,16 +5,25 @@ const taskList = document.getElementsByClassName('task_list')[0];
 const paramName_value = document.getElementsByClassName('task_value')[0];
 const param_submit = document.getElementsByClassName('task_submit')[0];
 const paramList = document.getElementById('paramList')[0];
+
+arr_paramList = ["やる気","コスト"]
+arr_taskList = [["運動",2,5],["勉強",3,4]]
+id_count = 1
 // 追加ボタンを作成
 const addTasks = (task) => {
   // 入力したタスクを追加・表示
   listItem = document.createElement('tr');
-  const head = document.createElement("th")
-  head.innerHTML = task
-  const data = document.createElement("td")
-  data.innerText = "data"
-  listItem.appendChild(head)
-  listItem.appendChild(data)
+  id_count += 1;
+  id = id_count;
+  listItem.setAttribute('id',id)
+  const head = document.createElement("th");
+  head.innerHTML = task[0];
+  listItem.appendChild(head);
+  for(i=1;i<task.length;i++){
+    data = document.createElement("td");
+    data.innerText = task[i];
+    listItem.appendChild(data)
+  }
   const showItem = taskList.appendChild(listItem);
 //   showItem.innerHTML = task;
 
@@ -39,14 +48,21 @@ const deleteTasks = (deleteButton) => {
 // 追加ボタンをクリックし、イベントを発動（タスクが追加）
 taskSubmit.addEventListener('click', evt => {
   evt.preventDefault();
-  const task = taskValue.value;
+  const taskName = taskValue.value;
+  var task = new Array(arr_paramList.length+1)
+  task[0] = taskName
   addTasks(task);
   taskValue.value = '';
 });
 
 taskValue.addEventListener('keypress', evt => {
     evt.preventDefault();
-    const task = taskValue.value;
+    const taskName = taskValue.value;
+    var task = new Array(arr_paramList.length+1)
+    task[0] = taskName
+    for(i=1;i<=arr_paramList.length;i++){
+        task[i] = 0
+    }
     addTasks(task);
     taskValue.value = '';
 });
