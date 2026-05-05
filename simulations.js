e77bd7f34bf5d0dec340a1e075df9f96fa28e61a
readerSpace <keks564976@gmail.com>
2026-05-05 23:55:17 +0900

    modified:   simulations.css 	modified:   simulations.js

--- simulations.js
+++ simulations.js
@@ -1,24 +1,3 @@
-const revealObserver = new IntersectionObserver(
-    (entries) => {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add("is-visible");
-                revealObserver.unobserve(entry.target);
-            }
-        });
-    },
-    {
-        threshold: 0.18,
-        rootMargin: "0px 0px -40px 0px"
-    }
-);
-
-document.querySelectorAll(".reveal, .site-footer").forEach((element) => {
-    if (!element.classList.contains("is-visible")) {
-        revealObserver.observe(element);
-    }
-});
-
 const filterButtons = document.querySelectorAll(".filter-chip");
 const simulationCards = document.querySelectorAll(".simulation-card");
 
